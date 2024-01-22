67ad102dedf9a6ee841814b33f8114acb657d2cc
nturner0 <nturner1@g.ucla.edu>
2024-01-21 21:25:29 -0800

    Updated object tracking to include RGB masking.

--- object_tracking.py
+++ object_tracking.py
@@ -33,42 +33,74 @@ while(True):
     # That didn't work either, experimentally hue = [5,15] works well, using that
 
     # The pencil is annoyingly close to skin hue, switching goal to a blue notebook
+    # The box tracks the notebook quite well, even with other objects in frame and different
+    # lighting configurations.
 
     # Capture frame-by-frame
     ret, frame = cap.read()
-    # HSV Image
+
+    # HSV and RGB Images
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    rgb = frame
 
     # Mask bounds
-    lower_bound = np.array([80,50,50])
-    upper_bound = np.array([110,255,255])
+    hsv_lower_bound = np.array([80,50,50])
+    hsv_upper_bound = np.array([110,255,255])
 
-    # Threshold the HSV image to get only pencil colors
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    rgb_lower_bound = np.array([0,150,0])
+    rgb_upper_bound = np.array([255,255,255])
 
-    # Get pencil contours, use mask since it is grayscale
-    ret,thresh = cv2.threshold(mask,127,255,0)
-    contours,hierarchy = cv2.findContours(thresh, 1, 2)
+    # Threshold the images to get only notebook colors
+    hsv_mask = cv2.inRange(hsv, hsv_lower_bound, hsv_upper_bound)
+    hsv_res = cv2.bitwise_and(frame,frame, mask= hsv_mask)
 
+    rgb_mask = cv2.inRange(rgb, rgb_lower_bound, rgb_upper_bound)
+    rgb_res = cv2.bitwise_and(frame,frame, mask= rgb_mask)
+
+    # Get notebook contours, use mask since it is grayscale
+    hsv_ret,hsv_thresh = cv2.threshold(hsv_mask,127,255,0)
+    hsv_contours,hsv_hierarchy = cv2.findContours(hsv_thresh, 1, 2)
+
+    rgb_ret,rgb_thresh = cv2.threshold(rgb_mask,127,255,0)
+    rgb_contours,rgb_hierarchy = cv2.findContours(rgb_thresh, 1, 2)
+
+    hsv_frame = frame.copy()
     # Ensure there is at least one contour
-    if len(contours) > 0:
-        cnt = contours[0]
+    if len(hsv_contours) > 0:
+        cnt = hsv_contours[0]
 
         # Find largest contour - this is likely the notebook outline
         max_area = 0
-        max_contour = contours[0]
-        for c in contours:
+        max_contour = hsv_contours[0]
+        for c in hsv_contours:
+            area = cv2.contourArea(c)
+            if (area > max_area):
+                max_area = area
+                max_contour = c
+        x,y,w,h = cv2.boundingRect(max_contour)
+        # Draw bounding rectangle
+        cv2.rectangle(hsv_frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+    rgb_frame = frame.copy()
+    # Ensure there is at least one contour
+    if len(rgb_contours) > 0:
+        cnt = rgb_contours[0]
+
+        # Find largest contour - this is likely the notebook outline
+        max_area = 0
+        max_contour = rgb_contours[0]
+        for c in rgb_contours:
             area = cv2.contourArea(c)
             if (area > max_area):
                 max_area = area
                 max_contour = c
         x,y,w,h = cv2.boundingRect(max_contour)
         # Draw bounding rectangle in original image
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.rectangle(rgb_frame,(x,y),(x+w,y+h),(0,255,0),2)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('HSV Masking', hsv_frame)
+    cv2.imshow('RGB Masking', rgb_frame)
     #cv2.imshow('mask',mask)
     #cv2.imshow('res',res)
     if cv2.waitKey(1) & 0xFF == ord('q'):
